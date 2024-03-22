39c6d908e84e4405a862013276eac51bea9c7487
Ayesha-chaudhry <ashijutt069@gmail.com>
2024-03-23 04:32:09 +0500

    new component

---
+++ src/components/Principles.tsx
@@ -0,0 +1,67 @@
+import Image from "next/image";
+import { BiChevronRight } from "react-icons/bi";
+
+export default function Principles() {
+  return (
+    <div className="bg-[#0b1729] h-full lg:h-screen">
+      <div className={"text-white text-4xl text-center font-bold"}>
+        Key Principles
+      </div>
+      <div className="text-gray-400 max-w-[650px] text-2xl text-center mx-auto mt-10">
+        Decentralised storage makes it nearly difficult for your data to be
+        hacked since files are encrypted and divided into parts client-side
+        before being spread over our network of high-performance Storage Nodes.
+      </div>
+      <div className="flex flex-wrap gap-y-14 lg:gap-8 max-w-[1500px] mx-10 lg:mx-auto mt-20">
+        {/* 1 */}
+        <div className="flex flex-col gap-5 border border-gray-700 p-8 rounded-3xl">
+          <Image src="/bg.jpeg" alt="img" width={200} height={200} />
+          <h1 className={"text-white text-4xl font-bold max-w-96"}>
+            Petabyte level of Distributed Data Sets
+          </h1>
+          <p className="text-gray-400 max-w-[400px] text-2xl ">
+            Datasets are distributed at the Petabye level to allow extremely
+            quick storage.
+            <div className="w-10 h-10  bg-[#0094ff] rounded-full relative mt-2">
+              <div className=" absolute top-1 left-1 text-3xl">
+                <BiChevronRight />
+              </div>
+            </div>
+          </p>
+        </div>
+        {/* 2 */}
+        <div className="flex flex-col gap-5 border border-gray-700 p-8 rounded-3xl">
+          <Image src="/bg.jpeg" alt="img" width={200} height={200} />
+          <h1 className={"text-white text-4xl font-bold max-w-96"}>
+            Petabyte level of Distributed Data Sets
+          </h1>
+          <p className="text-gray-400 max-w-[400px] text-2xl ">
+            Datasets are distributed at the Petabye level to allow extremely
+            quick storage.
+            <div className="w-10 h-10  bg-[#0094ff] rounded-full relative mt-2">
+              <div className=" absolute top-1 left-1 text-3xl">
+                <BiChevronRight />
+              </div>
+            </div>
+          </p>
+        </div>
+        {/* 3 */}
+        <div className="flex flex-col gap-5 border border-gray-700 p-8 rounded-3xl">
+          <Image src="/bg.jpeg" alt="img" width={200} height={200} />
+          <h1 className={"text-white text-4xl font-bold max-w-96"}>
+            Petabyte level of Distributed Data Sets
+          </h1>
+          <p className="text-gray-400 max-w-[400px] text-2xl ">
+            Datasets are distributed at the Petabye level to allow extremely
+            quick storage.
+            <div className="w-10 h-10  bg-[#0094ff] rounded-full relative mt-2">
+              <div className=" absolute top-1 left-1 text-3xl">
+                <BiChevronRight />
+              </div>
+            </div>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
